1e8dd402d329c03b3f38d1dbfaff1ac905c3254e
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2021-08-19 15:32:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,41 +1,25 @@
-variable "machine_type_val" {}
-variable "az_value" {}
-variable "project"{
-  default = "sbpuja"
-}
+
 variable "region" {
-  type        = string
-  default     = "asia-east1"
-
+    type = string
+    default = "us-central"
 }
+variable "project" {
+    type = string
+}
+
 variable "user" {
-
+    type = string
 }
-variable "email"{
 
+variable "email" {
+    type = string
 }
 variable "privatekeypath" {
-  default = "~/.ssh/id_rsa"
+    type = string
+    default = "~/.ssh/id_rsa"
 }
+
 variable "publickeypath" {
-  default = "~/.ssh/id_rsa.pub"
-}
-
-variable "vm_names" {
-  type    = list(any)
-  default = ["dev-vm", "stage-vm", "prod-vm"]
-}
-variable "istest" {}
-variable "tags" {
-  type    = list(any)
-  default = ["firstvm", "secondvm"]
-}
-variable "gvmtype" {
-  type = map(any)
-  default = {
-    asia-east1-a = "Debian GNU/Linux 10 (buster)"
-    asia-east1-b = "Debian GNU/Linux 9 (stretch)"
-    asia-east1-c = "centos-7-v20210721"
-
-  }
+    type = string
+    default = "~/.ssh/id_rsa.pub"
 }
