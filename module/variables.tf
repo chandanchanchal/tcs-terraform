abfbbe603d2a1554d79c7abf6a53df44fd0880f8
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2021-08-20 12:06:48 +0530

    Add files via upload

---
+++ module/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  default = "ckcterraform-module-demo1"
+  type    = string
+}
+
+variable "zone" {
+  default = "us-east1-b"
+  type    = string
+}
+
+variable "region" {
+  default = "us-east1"
+  type = string
+}
