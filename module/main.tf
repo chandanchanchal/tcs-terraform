8f9e42e44eccf03a2e591ae49c7d33340fa53346
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2021-08-20 12:11:00 +0530

    Delete module directory

--- module/main.tf
+++
@@ -1,7 +0,0 @@
-# Create the cluster
-resource "google_container_cluster" "gke-cluster" {
-  name               = "${var.cluster_name}"
-  network            = "default"
-  location           = "${var.zone}"
-  initial_node_count = 3
-}
